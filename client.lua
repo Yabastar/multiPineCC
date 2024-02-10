18133c83f9ad59694bcf1aba6bbcc62132c32fac
Yabastar <138197982+Yabastar@users.noreply.github.com>
2024-02-09 21:01:02 -0800

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,9 +1,93 @@
-rednet.open("top")
 local hostname = 0
+
+peripheral.find("modem", rednet.open)
 pine = require("/Pine3D")
+PrimeUI = require("/PrimeUI")
+PrimeUI.clear()
+PrimeUI.label(term.current(), 3, 2, "Sample Text")
+PrimeUI.horizontalLine(term.current(), 3, 3, #("Sample Text") + 2)
+
+local serverdata = {}
+
+local function search()
+    while true do
+        local id, msg = rednet.receive("MultiPineR")
+        if serverdata[id] == nil then
+            local devcount = 0
+            for _, v2 in pairs(textutils.unserialize(msg)) do
+                if v2 ~= nil then
+                    devcount = devcount + 1
+                end
+            end
+            serverdata[id] = devcount
+        end
+    end
+end
+
+local entries2 = {
+    "List servers",
+    "Connect with ID",
+}
+
+local entries2_descriptions = {
+    "List running MultiPine servers",
+    "Connect to a MultiPine server via computer ID"
+}
+
+local redraw = PrimeUI.textBox(term.current(), 3, 15, 40, 3, entries2_descriptions[1])
+PrimeUI.borderBox(term.current(), 4, 6, 40, 8)
+
+PrimeUI.selectionBox(term.current(), 4, 6, 40, 8, entries2, "done", function(option) redraw(entries2_descriptions[option]) end)
+
+local _, _, selection = PrimeUI.run()
+
+if selection == "List servers" then
+    local function parallelsearch()
+        while true do
+            search()
+        end
+    end
+
+    local function wait()
+        sleep(1)
+    end
+
+    local inverted = {}
+    
+    parallel.waitForAny(parallelsearch, wait)
+    
+    local entries = {}
+    local desc = {}
+
+    for i, v in pairs(serverdata) do
+        table.insert(entries, "Server " .. i .. " (" .. v .. " devices)")
+        table.insert(desc,"Join server")
+        inverted["Server " .. i .. " (" .. v .. " devices)"] = i
+    end
+
+    if #entries == 0 then
+        table.insert(entries, "Back")
+        table.insert(desc, "No servers found")
+    end
+
+    PrimeUI.clear()
+    PrimeUI.label(term.current(), 3, 2, "Sample Text")
+    PrimeUI.horizontalLine(term.current(), 3, 3, #("Sample Text") + 2)
+
+    local redrawServer = PrimeUI.textBox(term.current(), 3, 15, 40, 3, desc[1])
+    PrimeUI.borderBox(term.current(), 4, 6, 40, 8)
+    PrimeUI.selectionBox(term.current(), 4, 6, 40, 8, entries, "done", function(option) redrawServer(desc[option]) end)
+
+    local _, _, serverSelection = PrimeUI.run()
+    local index = inverted[serverSelection]
+    hostname = tonumber(index)
+end
+
 local frame = pine.newFrame()
 frame:setCamera(0, 6, 0, 0, 0, -90)
-local x,y,z = 0,0,0
+
+local x, y, z = 0, 0, 0
+
 local function runControls()
     while true do
         local event, key, _, _ = os.pullEvent()
@@ -20,7 +104,7 @@ local function runControls()
         elseif key == keys.q then
             y = y - 0.25
         end
-        rednet.send(hostname,{x,y,z})
+        rednet.send(hostname, { x, y, z },"MultiPine")
     end
 end
 
@@ -28,24 +112,32 @@ local objects = {}
 
 local function display()
     while true do
-        local _, res = rednet.receive("GS")
-        rednet.send(hostname, "received")
-        res = textutils.unserialize(res)
+        local id, res = rednet.receive("MultiPineR")
+        if id == hostname then
+            rednet.send(hostname, "received")
+            res = textutils.unserialize(res)
 
-        for i, v in pairs(res) do
-            -- Check if the object already exists in the objects table
-            local existingObject = objects[i]
+            for i, v in pairs(res) do
+                local existingObject = objects[i]
 
-            if existingObject then
-                existingObject:setPos(v[1], v[2], v[3])  -- Update position
-            else
-                local newObject = frame:newObject("models/box", v[1], v[2], v[3])
-                objects[i] = newObject
+                if existingObject then
+                    existingObject:setPos(v[1], v[2], v[3])
+                else
+                    local newObject = frame:newObject("models/box", v[1], v[2], v[3])
+                    objects[i] = newObject
+                end
             end
-        end
+            local allObj = {}
 
-        frame:drawObjects(objects)
-        frame:drawBuffer()
+            local count = 1
+            for k,v in pairs(objects) do
+                allObj[count] = v
+                count = count + 1
+            end
+            
+            frame:drawObjects(allObj)
+            frame:drawBuffer()
+        end
     end
 end
 
